36ef1f4f9981c521b4bea566f916278310c44da3
Ingo Hoffmann <ingoehoffmann@gmail.com>
2025-04-11 19:00:39 +0200

    start screen done
    parallax background and music

---
+++ scenes/main_title.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@onready var sprite = $BG/ParallaxLayer/Sprite2D
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene_to_file("res://scenes/town.tscn")
+
+	sprite.region_rect.position += delta * Vector2(15,15)
+	if sprite.region_rect.position >= Vector2(720, 743):
+		sprite.region_rect.position = Vector2.ZERO
